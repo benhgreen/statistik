28d220abe3bb8164bab530de43bf92759846e00f
Ben Green <ben.green@inventati.org>
2015-11-18 13:01:17 -0500

    lol

--- statistik/__init__.py
+++
@@ -1,4 +0,0 @@
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'statistik.settings'
-os.environ['DATABASE_URL'] = 'postgres://django:django@localhost/statistik_db'
-os.environ['PYTHONPATH'] = '/Users/ben/PycharmProjects/statistik'
