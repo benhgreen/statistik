fdcb9d7e8e410bf85b18643fe3cc4f304e90bab0
Ben Green <ben.green@inventati.org>
2015-11-30 17:06:05 -0500

    more deployment prep

--- statistik/settings.py
+++ statistik/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['statistik.benhgreen.com']
 
 
 # Application definition
