3d2613869766e6a3b799d6ec81ba1c3bbcbd393a
Ben Green <ben.green@inventati.org>
2015-12-28 17:13:45 -0500

    fix bug in admin

--- statistik/admin.py
+++ statistik/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from statistik.models import UserProfile, Chart, Song, Review, EloReview
 
-admin.site.register(UserProfile)
 admin.site.register(Chart)
 admin.site.register(Song)
 admin.site.register(Review)
 admin.site.register(EloReview)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    exclude = ['best_techniques']
