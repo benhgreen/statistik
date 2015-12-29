e7e9af8ae78a718d88d96e17942fddffda350a67
Ben Green <ben.green@inventati.org>
2015-12-29 13:18:06 -0500

    temp hide a user's reviews

--- statistik/controller.py
+++ statistik/controller.py
@@ -50,7 +50,7 @@ def get_avg_ratings(chart_ids, user_id=None):
                             ratings for that chart as well as a has_reviewed
                             boolean.
     """
-    matched_reviews = Review.objects.filter(chart__in=chart_ids)
+    matched_reviews = Review.objects.filter(chart__in=chart_ids).exclude(user_id=7)
     organized_reviews, reviewed_charts = organize_reviews(matched_reviews,
                                                           user_id=user_id)
     ret = {}
